78a33f15b596a3d6af1f99de9f1ed5e3f5ead169
Muhammad Sanan Sarwar <sanansarwar507@gmail.com>
2026-06-09 23:16:27 +0500

    Temporarily allow all origins

--- backend/src/main/java/com/algorithmrace/visualizer/CorsConfig.java
+++ backend/src/main/java/com/algorithmrace/visualizer/CorsConfig.java
@@ -1,12 +1,12 @@
 package com.algorithmrace.visualizer;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -21,7 +21,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                    .allowedOrigins(allowedOrigins)
+                    .allowedOrigins("*")
                     .allowedMethods("GET", "POST", "OPTIONS")
                     .allowedHeaders("*");
             }
